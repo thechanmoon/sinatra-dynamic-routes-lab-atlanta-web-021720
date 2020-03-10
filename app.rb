b532b09d5e8e62fa35c39edc9719de552ce00038
thechanmoon <thechanmoon@gmail.com>
2020-03-09 21:31:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -34,15 +34,16 @@ class App < Sinatra::Base
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     # @answer = params[:word1] +' '+ params[:word2] +' '+ params[:word3] +' '+ params[:word4] +' '+ params[:word5] 
-    # @answer = params[:word1] +' '+ params[:word2] +' '+ params[:word3] +' '+ params[:word4] +' '+ params[:word5]
+    @answer = params[:word1] +' '+ params[:word2] +' '+ params[:word3] +' '+ params[:word4] +' '+ params[:word5]+ '.'
 
     # h.each {|key, value| puts "#{key} is #{value}" }
-    @answer = ''
-    params.each do |key, value|
-      @answer = @answer + params[value]
-    end
+
+    # @answer = ''
+    # params.each do |key, value|
+    #   @answer = @answer + params[value] + ' '
+    # end
     
-    
+    # "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
